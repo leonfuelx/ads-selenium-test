2526eeb43e57e7ff517543803fb98229b357d348
leonfuelx <leon.wang@fuelx.com>
2019-07-16 10:25:20 -0700

    updated push
    	modified:   api.js
    	modified:   index.js

--- api.js
+++ api.js
@@ -1,146 +1,146 @@
-module.exports = (async function() {
-    const axios = require('axios');
-    const nodemailer = require('nodemailer');
+const axios = require('axios');
+const nodemailer = require('nodemailer');
 
-    const username = 'leonwang3';
-    const password = 'JjxqP423VEJqACjztxWj';
-        
-    let date = new Date().toString().slice(0, 15);
+const username = 'toolsoneteam1';
+const password = 'JjxqP423VEJqACjztxWj';
 
-    let browserStackAPI = `https://api.browserstack.com/automate/builds`,
-        buildId,
-        sessionId,
-        networkLogs,
-        client,
-        failedSessions = '';
-    
-    const options = {
-        url: `${browserStackAPI}.json`,
-        auth: {
-            username,
-            password
+const api = {};
+let date;
+
+let browserStackAPI = `https://api.browserstack.com/automate/builds`,
+    buildId,
+    sessionId,
+    networkLogs,
+    client,
+    failedSessions = '';
+
+const options = {
+    url: `${browserStackAPI}.json`,
+    auth: {
+        username,
+        password
+    }
+}
+
+const getBuild = async url => {
+    const data = await callApi(url);
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].automation_build.name === date) {
+            return data[i].automation_build.hashed_id;
         }
     }
-    
-    const getBuild = async url => {
-        const data = await callApi(url);
-        for (let i = 0; i < data.length; i++) {
-            if (data[i].automation_build.name === date) {
-                return data[i].automation_build.hashed_id;
-            }
-        }
+}
+
+const getSession = async url => {
+    sessionArr = [];
+    let data = await callApi(url);
+    data = data.build.sessions;
+    for (let i = 0; i < data.length; i++) {
+        sessionArr.push(data[i].automation_session.hashed_id);
     }
-    
-    const getSession = async url => {
-        sessionArr = [];
-        let data = await callApi(url);
-            data = data.build.sessions;
-        for (let i = 0; i < data.length; i++) {
-            sessionArr.push(data[i].automation_session.hashed_id);
-        }
-        return sessionArr;
-    } 
-    
-    
-    const getSessionData = async arr => {
-        for (let i = 0; i < arr.length; i++) {
-            try {
-                sessionId = arr[i];
-                options.url = `${browserStackAPI}/${buildId}/sessions/${sessionId}.json`
-                let data = await callApi(options.url);
-                networkLogs = `${data.automation_session.browser_url}/networklogs`
-                console.log('===========================================')
-                console.log('SESSIONID', sessionId)
-                let status = await getNetworkData(networkLogs);
-        
-                if (status) {
-                    failedSessions+=`<br>${client}: ${status}`
-                }
-                
-                options.url = '';
-            } catch(e) {
-                continue;
-            }
-        }
-        if (!failedSessions.length) {
-            sendMessage('All Clients Passed!')
-        } else {
-            sendMessage(failedSessions)
-        }
-    }
-    
-    const getNetworkData = async url => {
-        const data = await callApi(url)
-        let entries = data.log.entries;
-            client = entries[0].request.url;
-        let scriptExecutionTime = 0;
-        let fuelXCalls = 0;
-        for (let i = 0; i < entries.length; i++) {
-            let fuelx = entries[i];
-            if((fuelx.request.url.includes('fuel451.com') || fuelx.request.url.includes('fuelx.com')) && (fuelx.response.status === 200 || fuelx.response.status === 302 || fuelx.response.status === 301)) {
-                console.log(fuelx.request.url)
-                fuelXCalls++;
-                scriptExecutionTime += fuelx.time;
-            }
-            else if((fuelx.request.url.includes('fuel451.com') || fuelx.request.url.includes('fuelx.com')) && (fuelx.response.status !== 200 || fuelx.response.status !== 302) && !fuelx.request.url.includes('favicon')) {
-                return `Pixel: Endpoint failure (URL:${fuelx.request.url}, STATUS:${fuelx.response.status}), (sessionid: ${sessionId})`
-            }
-        }
-        
-        if(fuelXCalls < 5) {
-            return `Pixel: All Endpoints not called (${fuelXCalls}/10 calls), (sessionid: ${sessionId})`
-        } else if(scriptExecutionTime > 3000) {
-            return `Pixel: Execution took too long (${scriptExecutionTime}ms), (sessionid: ${sessionId})`;
-        } else if(scriptExecutionTime === 0) {
-            return `No Pixel (sessionid: ${sessionId})`;
-        } else {
-            console.log('Execution Time: ', scriptExecutionTime);
-        }
-    
-        return false;
-    }
-    
-    const callApi = async url => {
+    return sessionArr;
+}
+
+
+const getSessionData = async (arr, clientUrls) => {
+    for (let i = 0; i < arr.length; i++) {
         try {
-            const response = await axios.get(url, {auth: options.auth, headers: { 'Accept': 'application/json'}});
-            return response.data;
-        } catch (error) {
-            console.log('callAPI Error: ', error)
-        }
-    }
-    
-    const sendMessage = async (body) => {
-        let message = {
-            from:'leon.wang@fuelx.com',
-            to:['leon.wang@fuelx.com'],
-            subject: `${date} Pixel Check Issues`,
-            html: `<p>${body}</p>`
-        }
-    
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'leon.wang@fuelx.com',
-                pass: 'kgxxfarkeueljzsb'
-            } 
-        });
-    
-        transporter.sendMail(message, function(err, info) {
-            if(err)
-                console.log(err);
-            else
-                console.log(info);
-        })
-    }
-    
-    const initApi = async () => {
-        buildId = await getBuild(options.url);
-        options.url = `${browserStackAPI}/${buildId}.json`
-        sessionIdArr = await getSession(options.url);
-        sessionIdArr = sessionIdArr.reverse();
-        await getSessionData(sessionIdArr);
-    }
-    
-    initApi();
+            client = clientUrls[i];
+            sessionId = arr[i];
+            options.url = `${browserStackAPI}/${buildId}/sessions/${sessionId}.json`
+            let data = await callApi(options.url);
+            networkLogs = `${data.automation_session.browser_url}/networklogs`
+            console.log('===========================================')
+            console.log('SESSIONID', sessionId)
+            let status = await getNetworkData(networkLogs);
 
-})()
+            if (status) {
+                failedSessions += `<br>${client}: ${status}`
+            }
+
+            options.url = '';
+        } catch (e) {
+            continue;
+        }
+    }
+    if (!failedSessions.length) {
+        sendMessage('All Clients Passed!')
+    } else {
+        sendMessage(failedSessions)
+    }
+}
+
+const getNetworkData = async url => {
+    const data = await callApi(url)
+    let entries = data.log.entries;
+    // client = entries[0].request.url;
+    let scriptExecutionTime = 0;
+    let fuelXCalls = 0;
+    for (let i = 0; i < entries.length; i++) {
+        let fuelx = entries[i];
+        if ((fuelx.request.url.includes('fuel451.com') || fuelx.request.url.includes('fuelx.com')) && (fuelx.response.status === 200 || fuelx.response.status === 302 || fuelx.response.status === 301)) {
+            console.log(fuelx.request.url)
+            fuelXCalls++;
+            scriptExecutionTime += fuelx.time;
+        }
+        else if ((fuelx.request.url.includes('fuel451.com') || fuelx.request.url.includes('fuelx.com')) && (fuelx.response.status !== 200 || fuelx.response.status !== 302) && !fuelx.request.url.includes('favicon')) {
+            return `Pixel: Endpoint failure (URL:${fuelx.request.url}, STATUS:${fuelx.response.status}), (sessionid: ${sessionId})`
+        }
+    }
+
+    if (scriptExecutionTime === 0) {
+        return `No Pixel (sessionid: ${sessionId})`;
+    } else if (fuelXCalls < 5) {
+        return `Pixel: All Endpoints not called (${fuelXCalls}/7 calls), (sessionid: ${sessionId})`
+    } else if (scriptExecutionTime > 5000) {
+        return `Pixel: Execution took too long (${scriptExecutionTime}ms), (sessionid: ${sessionId})`;
+    } else {
+        console.log('Execution Time: ', scriptExecutionTime);
+    }
+
+    return false;
+}
+
+const callApi = async url => {
+    try {
+        const response = await axios.get(url, { auth: options.auth, headers: { 'Accept': 'application/json' } });
+        return response.data;
+    } catch (error) {
+        console.log('callAPI Error: ', error)
+    }
+}
+
+const sendMessage = async (body) => {
+    let message = {
+        from: 'leon.wang@fuelx.com',
+        to: ['leon.wang@fuelx.com', 'santosh@fuelx.com'],
+        subject: `${date} Pixel Check Issues`,
+        html: `<p>${body}</p>`
+    }
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'leon.wang@fuelx.com',
+            pass: 'kgxxfarkeueljzsb'
+        }
+    });
+
+    transporter.sendMail(message, function (err, info) {
+        if (err)
+            console.log(err);
+        else
+            console.log(info);
+    })
+}
+
+api.initApi = async (clientUrls, testDate) => {
+    date = testDate;
+    buildId = await getBuild(options.url);
+    options.url = `${browserStackAPI}/${buildId}.json`
+    sessionIdArr = await getSession(options.url);
+    sessionIdArr = sessionIdArr.reverse();
+    await getSessionData(sessionIdArr, clientUrls);
+}
+
+module.exports = api;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const webdriver = require('selenium-webdriver');
 const mySql = require('./mysql.js');
+const pixelTest = require('./api.js');
 
 const util = require('util');
 const CronJob = require('cron').CronJob;
@@ -7,139 +8,139 @@ const CronJob = require('cron').CronJob;
 require('./fast-selenium.js');
 
 
-new CronJob('0 0 5 * * *', function() {
+// new CronJob('0 0 5 * * *', function() {
+let date = new Date().toString().slice(0, 24);
 
-  let activeLandingPages = `select distinct clickurl from fuelData.bidding where campaignStatus = 4`
+let activeLandingPages = `select distinct clickurl from fuelData.bidding where campaignStatus = 4`
 
-  let queryExecution = util.promisify(mySql.query).bind(mySql);
-  let noDuplicateSites = [];
+let queryExecution = util.promisify(mySql.query).bind(mySql);
+let noDuplicateSites = [];
 
-  async function dbInit() {
-    try {
-      let landingPages = await queryExecution(activeLandingPages)
-      landingPages = landingPages.map(el => el.clickurl);
+async function dbInit() {
+  try {
+    let landingPages = await queryExecution(activeLandingPages)
+    landingPages = landingPages.map(el => el.clickurl);
 
-      for (let i = 0; i < landingPages.length; i++) {
-        if (landingPages[i].indexOf('?') > 0) {
-          let index = landingPages[i].indexOf('?');
-          noDuplicateSites.push(landingPages[i].slice(0,index));
-        } else if (landingPages[i][landingPages[i].length-1] === '/') {
-          noDuplicateSites.push(landingPages[i].slice(0,landingPages[i].length-1))
-        } else {
-          noDuplicateSites.push(landingPages[i]);
-        }
+    for (let i = 0; i < landingPages.length; i++) {
+      if (landingPages[i].indexOf('?') > 0) {
+        let index = landingPages[i].indexOf('?');
+        noDuplicateSites.push(landingPages[i].slice(0, index));
+      } else if (landingPages[i][landingPages[i].length - 1] === '/') {
+        noDuplicateSites.push(landingPages[i].slice(0, landingPages[i].length - 1))
+      } else {
+        noDuplicateSites.push(landingPages[i]);
       }
-      noDuplicateSites = Array.from(new Set([...noDuplicateSites]));
-      console.log('Live Clients: ');
-      console.log('No Duplicate Sites', noDuplicateSites);
-    } finally {
-      mySql.end();
-      console.log('db closed')
     }
+    noDuplicateSites = Array.from(new Set([...noDuplicateSites]));
+    console.log('Live Clients: ');
+    console.log('No Duplicate Sites', noDuplicateSites);
+  } finally {
+    mySql.end();
+    console.log('db closed')
   }
-
-  let date = new Date().toString().slice(0, 15);
-
-  // Input capabilities
-  const capabilities = {
-    'browserName': 'Safari',
-    'browser_version': '12.0',
-    'os': 'OS X',
-    'os_version': 'Mojave',
-    'resolution': '1920x1080',
-    'project': 'Client_Pixel_Check',
-    'build': date,
-    'browserstack.user': 'leonwang3',
-    'browserstack.key': 'JjxqP423VEJqACjztxWj',
-    'browserstack.networkLogs': 'true',
-    'acceptSslCerts': 'true',
-    'browserstack.debug': 'true',
-  }
+}
 
 
+// Input capabilities
+const capabilities = {
+  'browserName': 'Safari',
+  'browser_version': '12.0',
+  'os': 'OS X',
+  'os_version': 'Mojave',
+  'resolution': '1920x1080',
+  'project': 'Client_Pixel_Check',
+  'build': date,
+  'browserstack.user': 'toolsoneteam1',
+  'browserstack.key': 'JjxqP423VEJqACjztxWj',
+  'browserstack.networkLogs': 'true',
+  'acceptSslCerts': 'true',
+  'browserstack.debug': 'true',
+}
 
-  async function appleMacTests() {
-    try {
-      let appleOs = ['Mojave'];
-      let browser = ['Chrome'];
-      let chromeVersion = ['70']
-    
-      capabilities.os = 'OS X';
-    
-      for (let i = 0; i < appleOs.length; i++) {
-        capabilities.os_version = appleOs[i];
-        for (let j = 0; j < browser.length; j++) {
-          capabilities.browserName = browser[j];
-          if (browser[j] === 'Safari') {
-            switch (appleOs[i]) {
-              case 'Mojave':
-                capabilities.browser_version = '12'
-                break;
-              case 'High Sierra':
-                capabilities.browser_version = '11.1'
-                break;
-              case 'Sierra':
-                capabilities.browser_version = '10'
-                break;
-              case 'El Capitan':
-                capabilities.browser_version = '9.1'
-                break;
-              default:
-                break;
-            }
-    
+
+
+async function appleMacTests() {
+  try {
+    let appleOs = ['Mojave'];
+    let browser = ['Chrome'];
+    let chromeVersion = ['75']
+
+    capabilities.os = 'OS X';
+
+    for (let i = 0; i < appleOs.length; i++) {
+      capabilities.os_version = appleOs[i];
+      for (let j = 0; j < browser.length; j++) {
+        capabilities.browserName = browser[j];
+        if (browser[j] === 'Safari') {
+          switch (appleOs[i]) {
+            case 'Mojave':
+              capabilities.browser_version = '12'
+              break;
+            case 'High Sierra':
+              capabilities.browser_version = '11.1'
+              break;
+            case 'Sierra':
+              capabilities.browser_version = '10'
+              break;
+            case 'El Capitan':
+              capabilities.browser_version = '9.1'
+              break;
+            default:
+              break;
+          }
+
+          for (let z = 0; z < noDuplicateSites.length; z++) {
+            // console.log(noDuplicateSites[z])
+            await buildDriver(noDuplicateSites[z])
+            // console.log(noDuplicateSites[z], capabilities)
+          }
+        }
+
+        if (browser[j] === 'Chrome') {
+          for (let y = 0; y < chromeVersion.length; y++) {
+            capabilities.browser_version = chromeVersion[y];
             for (let z = 0; z < noDuplicateSites.length; z++) {
-              // console.log(noDuplicateSites[z])
               await buildDriver(noDuplicateSites[z])
               // console.log(noDuplicateSites[z], capabilities)
             }
           }
-    
-          if (browser[j] === 'Chrome') {
-            for (let y = 0; y < chromeVersion.length; y++) {
-              capabilities.browser_version = chromeVersion[y];
-              for (let z = 0; z < noDuplicateSites.length; z++) {
-                await buildDriver(noDuplicateSites[z])
-                // console.log(noDuplicateSites[z], capabilities)
-              }
-            }
-          }
-    
         }
+
       }
-
-    } catch(error) {
-      console.error('ERROR in AppleTests: ', error);
     }
 
+  } catch (error) {
+    console.error('ERROR in AppleTests: ', error);
   }
 
-  async function buildDriver(client) {
-    try {
-      let driver = new webdriver.Builder().
-        usingServer('http://hub-cloud.browserstack.com/wd/hub').
-        withCapabilities(capabilities).
-        build();
-    
-      await driver.get(`${client}`).then(function () {
-        driver.getTitle().then(function (title) {
-          console.log(title);
-          setTimeout(() => { driver.quit(); }, 5000)
-        })
+}
+
+async function buildDriver(client) {
+  try {
+    let driver = new webdriver.Builder().
+      usingServer('http://hub-cloud.browserstack.com/wd/hub').
+      withCapabilities(capabilities).
+      build();
+
+    await driver.get(`${client}`).then(function () {
+      driver.getTitle().then(function (title) {
+        console.log(title);
+        setTimeout(() => { driver.quit(); }, 5000)
       })
+    })
 
-    } catch(error) {
-      console.error(error);
-    }
+  } catch (error) {
+    console.error(error);
   }
+}
 
-  async function testInit() {
-    await dbInit();
-    await appleMacTests();
-    setTimeout(function() {
-      require('./api.js');
-    }, 30000)
-  }
+async function testInit() {
+  await dbInit();
+  await appleMacTests();
+  setTimeout(function () {
+    pixelTest.initApi(noDuplicateSites, date)
+  }, 30000)
+}
 
-  testInit();
-}, null, true, 'America/Los_Angeles');
+testInit();
+// }, null, true, 'America/Los_Angeles');
